b8996a6da3c8f00ea3f33de43a7221d1fd9cf5d5
wangzw <wzw571603974@qq.com>
2025-11-12 16:52:11 +0800

    Initial Commit

---
+++ PulseFocus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PulseFocus
+//
+//  Created by Tuple on 2025/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PulseFocus/PulseFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseFocusApp.swift
+//  PulseFocus
+//
+//  Created by Tuple on 2025/11/12.
+//
+
+import SwiftUI
+
+@main
+struct PulseFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
