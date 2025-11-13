57389de1a6a1fa4ee63862cdb9905df4f17192b9
wangzw <wzw571603974@qq.com>
2025-11-13 13:56:06 +0800

    feat: 初始化WatchPulseFocus Watch应用项目
    添加基础项目文件，包括应用图标、主程序入口、内容视图和测试文件

---
+++ WatchPulseFocus Watch App/WatchPulseFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchPulseFocusApp.swift
+//  WatchPulseFocus Watch App
+//
+//  Created by Tuple on 2025/11/13.
+//
+
+import SwiftUI
+
+@main
+struct WatchPulseFocus_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
